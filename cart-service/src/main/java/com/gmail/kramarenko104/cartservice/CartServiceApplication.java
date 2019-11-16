f8a6355fc5fac88804be92f50b84fdd121f2b0c2
Iuliia Kramarenko <kramarenko104@gmail.com>
2019-11-16 22:49:21 +0200

    Add Swagger2 for API documentation

--- cart-service/src/main/java/com/gmail/kramarenko104/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/com/gmail/kramarenko104/cartservice/CartServiceApplication.java
@@ -8,10 +8,17 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableSwagger2
 public class CartServiceApplication {
 
 	@Bean
@@ -26,5 +33,16 @@ public class CartServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CartServiceApplication.class, args);
 	}
+
+	@Bean
+	public Docket swaggerApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("pl.piomin.services.employee.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(new ApiInfoBuilder().version("1.0").title("Employee API").description("Documentation Employee API v1.0").build());
+	}
+
 
 }
