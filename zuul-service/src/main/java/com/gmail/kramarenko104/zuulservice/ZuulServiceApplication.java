5817626d0c83fa98cb706291e75bbd630f81922c
Iuliia Kramarenko <kramarenko104@gmail.com>
2019-11-24 08:29:17 +0200

    Zuul service added

---
+++ zuul-service/src/main/java/com/gmail/kramarenko104/zuulservice/ZuulServiceApplication.java
@@ -0,0 +1,19 @@
+package com.gmail.kramarenko104.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2
+@EnableZuulProxy
+public class ZuulServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServiceApplication.class, args);
+    }
+
+}
