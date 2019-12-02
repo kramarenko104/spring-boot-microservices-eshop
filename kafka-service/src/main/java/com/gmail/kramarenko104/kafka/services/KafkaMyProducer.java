83d62839f6cb348b7da8002610ced566ab2fe425
julia_kramarenko <kramarenko104@gmail.com>
2019-12-02 13:31:17 +0200

    Add callback to Kafka Producer

--- kafka-service/src/main/java/com/gmail/kramarenko104/kafka/services/KafkaMyProducer.java
+++ kafka-service/src/main/java/com/gmail/kramarenko104/kafka/services/KafkaMyProducer.java
@@ -1,8 +1,7 @@
 package com.gmail.kramarenko104.kafka.services;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,14 +22,14 @@ public class KafkaMyProducer {
     private Producer<String, String> producer;
 
     public void configure(){
+        // Defining producer properties
         Properties producerProperties = new Properties();
-        /* Defining producer properties */
-        producerProperties.put("bootstrap.servers", kafkaBootstrapServers);
-        producerProperties.put("acks", "all");  // acknowledge from leader and from replicas
-        producerProperties.put("retries", 2);   // after 2 unsuccessful retries sending becomes failed
-        producerProperties.put("linger.ms", 1); // every millisecond record will be sent
-        producerProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        producerProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers);
+        producerProperties.put(ProducerConfig.ACKS_CONFIG, "all");  // acknowledge from leader and from replicas
+        producerProperties.put(ProducerConfig.RETRIES_CONFIG, 2);   // after 2 unsuccessful retries sending becomes failed
+        producerProperties.put(ProducerConfig.LINGER_MS_CONFIG, 1); // every millisecond record will be sent
+        producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         producer = new KafkaProducer<String, String>(producerProperties);
     }
@@ -38,7 +37,19 @@ public class KafkaMyProducer {
     public void sendMessage(String  key, String message) {
         logger.debug(String.format("[eshop] #### -> Producing message -> %s", message));
         ProducerRecord<String, String> record = new ProducerRecord<>(MY_TOPIC, key, message);
-        producer.send(record);
+
+        producer.send(record, (recordMetadata, e) -> {
+            if (e == null) {
+                logger.info("[eshop] #### -> Received new metadata from Kafka: \n"+
+                        "Topic: " + recordMetadata.topic() +
+                        ", Partition: " + recordMetadata.partition() +
+                        ", Offset: " + recordMetadata.offset() +
+                        ", Timestamp: " + recordMetadata.timestamp());
+            } else {
+                logger.error("[eshop] #### -> Error while producing message: " + e.getMessage());
+            }
+        });
+        // flush and close producer
         producer.close();
     }
 
